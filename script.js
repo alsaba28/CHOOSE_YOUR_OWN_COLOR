d59c22ce777e8bdaed508a796faba6734368d0ae
Abdulla Al Saba <abdullaalsaba28@gmail.com>
2026-02-15 01:34:27 +0600

    choose your own color project

---
+++ script.js
@@ -0,0 +1,101 @@
+function pink() {
+  document.body.style.backgroundColor = "pink";
+}
+function lightPink(){
+    document.body.style.backgroundColor = 'lightpink';
+}
+function hotPink(){
+    document.body.style.backgroundColor = 'hotpink';
+}
+function peachPuff(){
+    document.body.style.backgroundColor = 'peachpuff';
+}
+function mistyRose(){
+    document.body.style.backgroundColor = 'mistyrose';
+}
+function lavender(){
+    document.body.style.backgroundColor = 'lavender';
+}
+function plum(){
+    document.body.style.backgroundColor = 'plum';
+}
+function violet(){
+    document.body.style.backgroundColor = 'violet';
+}
+function thistle(){
+    document.body.style.backgroundColor = 'thistle';
+}
+function skyBlue(){
+    document.body.style.backgroundColor = 'skyblue';
+}
+function powderBlue(){
+  document.body.style.backgroundColor = 'powderblue';
+}
+
+function lightBlue(){
+    document.body.style.backgroundColor = 'lightblue';
+}
+function paleTurquoise(){
+    document.body.style.backgroundColor = 'paleturquoise';
+}
+function mintCream(){
+    document.body.style.backgroundColor = 'mintcream';
+}
+function honeyDew(){
+    document.body.style.backgroundColor = 'honeydew';
+}
+function lemonChiffon(){
+    document.body.style.backgroundColor = 'lemonchiffon';
+}
+function lightYellow(){
+    document.body.style.backgroundColor = 'lightyellow';
+}
+function papayaWhip(){
+    document.body.style.backgroundColor = 'papayawhip';
+}
+function cornSilk(){
+    document.body.style.backgroundColor = 'cornsilk';
+}
+function wheat(){
+    document.body.style.backgroundColor = 'wheat';
+}
+function lightSalmon(){
+    document.body.style.backgroundColor = 'lightsalmon';
+}
+function coral(){
+    document.body.style.backgroundColor = 'coral';
+}
+function salmon(){
+    document.body.style.backgroundColor = 'salmon';
+}
+function lightGreen(){
+    document.body.style.backgroundColor = 'lightgreen';
+}
+function paleGreen(){
+    document.body.style.backgroundColor = 'palegreen';
+}
+function lightCoral(){
+    document.body.style.backgroundColor = 'lightcoral';
+}
+function deepPink(){
+    document.body.style.backgroundColor = 'deeppink';
+}
+function orchid(){
+    document.body.style.backgroundColor = 'orchid';
+}
+function mediumPurple(){
+    document.body.style.backgroundColor = 'mediumpurple';
+}
+function aqua(){
+    document.body.style.backgroundColor = 'aqua';
+}
+function lightSkyBlue(){
+    document.body.style.backgroundColor = 'lightskyblue';
+}
+function khaki(){
+    document.body.style.backgroundColor = 'khaki';
+}
+function maroon() {
+  document.body.style.backgroundColor = "maroon";
+}
+
